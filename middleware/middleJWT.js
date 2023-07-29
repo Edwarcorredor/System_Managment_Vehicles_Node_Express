a17e5d0dddd9bb527c21d14a94e92a051334bd43
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-07-29 03:58:42 -0500

    feat: :sparkles: Comprobacion TOKEN
    Se implementa la comprobación del token de la tabla, solo se puede generar un token si existe esa tabla dentro de la base de datos y luego se valida que ese token corresponde a la tabla para la que fue creado

--- middleware/middleJWT.js
+++ middleware/middleJWT.js
@@ -2,98 +2,58 @@ import dotenv from 'dotenv'
 import { SignJWT, jwtVerify } from 'jose';
 import express from 'express';
 import 'reflect-metadata';
-import {plainToClass, classToPlain } from 'class-transformer';
-import {Alarmas} from '../controller/Alarmas.js'
-import {ClasesAlarmas} from '../controller/ClasesAlarmas.js'
-import {Empresas} from '../controller/Empresas.js'
-import {Mantenimientos} from '../controller/Mantenimientos.js'
-import {Marcas} from '../controller/Marcas.js'
-import {Modelos} from '../controller/Modelos.js'
-import {Proveedores} from '../controller/Proveedores.js'
-import {RegistrosMantenimientos} from '../controller/RegistrosMantenimientos.js'
-import {SucursalesProveedores} from '../controller/SucursalesProveedores.js'
-import {Vehiculos} from '../controller/Vehiculos.js'
+
 
 const tokenJWT = express();
 const validateJWT = express();
 dotenv.config("../");
 
-tokenJWT.use(async(req,res,next)=>{
-    let inst;
-    switch (req.query.tabla) {
-        case 'alarmas':
-            inst = plainToClass(Alarmas, {}, { ignoreDecorators: true })
-            break;
-        
-        case 'clases_alarmas':
-            inst = plainToClass(ClasesAlarmas, {}, { ignoreDecorators: true })
-            break;
-        
-        case 'empresas':
-            inst = plainToClass(Empresas, {}, { ignoreDecorators: true })
-            break;
+const tablas = {
+    "alarmas": "alarmas",
+    "clases_alarmas": "clases_alarmas",
+    "empresas": "empresas",
+    "mantenimientos":"mantenimientos",
+    "marcas":"marcas",
+    "modelos":"modelos",
+    "proveedores":"proveedores",
+    "registros_mantenimientos":"registros_mantenimientos",
+    "sucursales_proveedores":"sucursales_proveedores",
+    "vehiculos":"vehiculos"
+}
 
-        case 'mantenimientos':
-            inst = plainToClass(Mantenimientos, {}, { ignoreDecorators: true })
-            break;
-        
-        case 'marcas':
-            inst = plainToClass(Marcas, {}, { ignoreDecorators: true })
-            break;
-        
-        case 'modelos':
-            inst = plainToClass(Modelos, {}, { ignoreDecorators: true })
-            break;
-
-        case 'proveedores':
-            inst = plainToClass(Proveedores, {}, { ignoreDecorators: true })
-            break;
-
-        case 'registros_mantenimientos':
-            inst = plainToClass(RegistrosMantenimientos, {}, { ignoreDecorators: true })
-            break;
-
-        case 'sucursales_proveedores':
-            inst = plainToClass(SucursalesProveedores, {}, { ignoreDecorators: true })
-            break;
-
-        case 'vehiculos':
-            inst = plainToClass(Vehiculos, {}, { ignoreDecorators: true })
-            break;
-
-        default:
-            res.json({status: 406, message: "No se puede generar el token"});
-            break;
-    }
-   
-    let interfaceData = classToPlain(inst);
+tokenJWT.use(async(req,res)=>{
+    if (tablas[req.query.tabla] === undefined){
+        return res.status(406).send('Error al generar token, especifique una tabla válida')
+    } 
     const encoder = new TextEncoder();
-    const jwtconstructor = new SignJWT({interfaceData});
-    
-    const jwt = await jwtconstructor
-    .setProtectedHeader({alg:"HS256", typ: "JWT"})
-    .setIssuedAt()
-    .setExpirationTime("25m")
-    .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
-    req.data = jwt;
-    next();
+    const jwtConstructor = await new SignJWT({ TablaCreada: tablas[req.query.tabla] })
+        .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+        .setIssuedAt()
+        .setExpirationTime('1h')
+        .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
+
+    res.send(jwtConstructor);
 })
+
 validateJWT.use(async(req,res,next)=>{
-    const {authorization} = req.headers;
-    if (!authorization) return res.json({status: 401, message: "Token no enviado"});
+    const { authorization } = req.headers;
+    if (!authorization) return res.status(404).send('Falta el token de autorización');
     try {
         const encoder = new TextEncoder();
         const jwtData = await jwtVerify(
             authorization,
             encoder.encode(process.env.JWT_PRIVATE_KEY)
         );
-        req.data = jwtData.payload;
+        if (!(jwtData.payload.TablaCreada === req.baseUrl.split('/')[1])) return res.status(401).send('Token incorrecto para esta tabla');
+        req.payloadJWT = jwtData.payload; 
         next();
     } catch (error) {
-        res.json({status: 401, message: "Token caducado"});
+        res.status(401).send('No autorizado');
+        console.log(error);
     }
 })
 export {
     tokenJWT,
     validateJWT
 };
+
